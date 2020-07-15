41217cd659146923006b29c0fa24554073007fbd
Kai Wang <kxw367@case.edu>
2020-07-15 16:46:28 -0400

    Complete basic functionality for starting task

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, View } from 'react-native';
+import HomePage from './components/Screens/HomePage'
+import ChooseMode from './components/Screens/ChooseMode'
+import TaskPage from './components/Screens/TaskPage';
+import Timer from './components/Screens/Timer';
+import StatusReport from './components/Screens/StatusReport';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-       Hello World
-      </Text>
-    </View>
-  );
-}
+
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Home"
+          component={HomePage}
+          options={{ title: 'Welcome' }}
+        />
+      <Stack.Screen name="Tasks" component={TaskPage} />
+      <Stack.Screen name="Choose Mode" component={ChooseMode} />
+      <Stack.Screen name="Timer" component={Timer} />
+      <Stack.Screen name="Status Report" component={StatusReport}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
+
 
 export default App;
---
+++ components/Screens/Timer.js
@@ -0,0 +1,30 @@
+//@todo Michael please integrate the timer component there.
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+
+const Timer = ({navigation}) => {
+  return(
+    <View style={styles.header}>
+      <Text style={styles.text}>Timer starts!</Text>
+      <Button title = "End Timer" color="red" 
+           onPress={() => navigation.navigate('Status Report')} />
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  header:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'darkslateblue'
+  },
+  text:{
+      color:'#fff',
+      fontSize: 30,
+      textAlign: 'center'
+  }
+
+})
+
+export default Timer;
