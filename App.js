0a2dd45b1637d85085686952c8f0be59f2129c82
weijili <weijili@umich.edu>
2020-07-16 19:43:43 +0800

    Install dependencies and create simple navigation

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const App = () => {
+function HelloWorld(){
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>
        Hello World
       </Text>
     </View>
+  );
+}
+
+function IamBreakOut(){
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>
+       I am Break Out
+      </Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Hello" component={HelloWorld} />
+      <Tab.Screen name="I am" component={IamBreakOut} />
+    </Tab.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
