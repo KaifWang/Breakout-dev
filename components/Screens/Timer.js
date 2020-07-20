a78e495e33b505c1ac669ce341c6e3ebe3c3a123
唐溢知 <tangyizhi@tangyizhideMacBook-Pro.local>
2020-07-20 14:07:45 +0800

    Pop Up Window of Timer Screen: Place your device face down to start timing.

--- components/Screens/Timer.js
+++ components/Screens/Timer.js
@@ -1,11 +1,39 @@
 //@todo Michael please integrate the timer component there.
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, { useState } from "react";
+import {Alert, Modal, StyleSheet, Image, ImageBackground, Text, TouchableHighlight, View, Button} from "react-native";
 
 const Timer = ({navigation}) => {
+  //modalVisible, a boolean variable, is the current visibility status of the pop-up window. true->show, false->hide
+  const [modalVisible, setModalVisible] = useState(true);
+
   return(
     <View style={styles.header}>
       <Text style={styles.text}>Timer starts!</Text>
+
+
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+      <ImageBackground style={styles.PopUpImage} source = {require('../../src/Alert.png')}>
+        <TouchableHighlight
+          onPress={() => {
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <Image style={styles.PopUpButtonImage} source = {require('../../src/OK.png')}></Image>
+        </TouchableHighlight>
+      </ImageBackground>
+
+      </Modal>
+
+
+
       <Button title = "End Timer" color="red" 
            onPress={() => navigation.navigate('Status Report')} />
     </View>
@@ -23,8 +51,19 @@ const styles = StyleSheet.create({
       color:'#fff',
       fontSize: 30,
       textAlign: 'center'
+  },
+  PopUpImage: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 220,
+    marginLeft: "5%",
+    width: 400,
+    height: 400
+  },
+  PopUpButtonImage:{
+    marginTop: 180,
+    width: 400,
+    height: 400
   }
-
-})
-
+});
 export default Timer;
