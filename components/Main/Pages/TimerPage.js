b335c9a6c02c989f014517a4792102656e3576de
Kai Wang <kxw367@case.edu>
2020-07-24 15:21:23 -0400

    Style timer page according to Katie's design

--- components/Main/Pages/TimerPage.js
+++ components/Main/Pages/TimerPage.js
@@ -1,30 +1,55 @@
-//@todo Michael please integrate the timer component there.
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, Dimensions,Image} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const TimerPage = ({navigation}) => {
-  return(
-    <View style={styles.header}>
-      <Text style={styles.text}>Timer starts!</Text>
-      <Button title = "End Timer" color="red" 
-           onPress={() => navigation.navigate('Status Report')} />
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+          colors={['#C8E0E1', 'white']}
+          style={styles.linearGradient}
+        >
+        <Text style={styles.time}>01:26</Text>
+        <TouchableOpacity style = {styles.button} onPress={() => navigation.navigate('Status Report')}>
+          <Image style = {styles.image}
+          source={require('../../../images/TimerButton.png')}></Image>
+        </TouchableOpacity>
+       
+      </LinearGradient>
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
-  header:{
-    flex:1,
+  container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'darkslateblue'
   },
-  text:{
-      color:'#fff',
-      fontSize: 30,
-      textAlign: 'center'
+  button: {
+    width:"17%",
+    height:"12%",
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  image:{
+    flex:1,
+    width:300,
+    height:300
+  },
+  linearGradient:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex:1,
+    width: Dimensions.get('window').width
+  },
+  time:{
+    paddingBottom:250,
+    fontFamily:'GillSans-Light',
+    fontSize:40,
+    color:'#83ACB2'
   }
-
-})
+});
 
 export default TimerPage;
+
