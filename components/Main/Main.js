7ace269ad8fa3a34aeeeb093c46baa4d69d7a8ce
Yizhi Tang <tangyizhi2000@outlook.com>
2020-08-03 16:44:21 +0800

    NewMeeting Page after Detailed Fix

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -4,6 +4,7 @@ import ChooseModePage from './Pages/ChooseModePage';
 import TaskPage from './Pages/TaskPage';
 import TimerPage from './Pages/TimerPage';
 import Timer from './TimerUtil/TimerClock';
+import NewMeeting from './Pages/NewMeeting';
 import StatusReportPage from './Pages/StatusReportPage';
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -13,6 +14,7 @@ const Main = () => {
   return (
     <Stack.Navigator headerMode="none">
       <Stack.Screen name="Home" component={StartSessionPage} />
+      <Stack.Screen name="NewMeeting" component={NewMeeting} />
       <Stack.Screen name="Tasks" component={TaskPage} />
       <Stack.Screen name="Timer" component={Timer} />
       <Stack.Screen name="Status Report" component={StatusReportPage} />
