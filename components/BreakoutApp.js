1244cda79d4d46e30fd9aa846cfadc4fce79b351
weijili <weijili@umich.edu>
2020-08-16 00:31:00 +0800

    add bottom tab icon

--- components/BreakoutApp.js
+++ components/BreakoutApp.js
@@ -4,6 +4,7 @@ import Setting from './Setting/Setting';
 import Community from './Community/Community';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Image, StyleSheet} from "react-native";
 
 const Tab = createBottomTabNavigator();
 
@@ -20,17 +21,39 @@ function getTabBarVisible(route) {
 function BreakoutApp() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator tabBarOptions={{activeTintColor: "#DCA392"}}>
         <Tab.Screen name="Main" 
         component={Main} 
         options={({ route }) => ({
           tabBarVisible: getTabBarVisible(route),
         })}/>
-        <Tab.Screen name="Community" component={Community} />
-        <Tab.Screen name="Setting" component={Setting} />
+        <Tab.Screen 
+          name="Community" 
+          component={Community} 
+          options={{
+            tabBarIcon: () => (
+              <Image source={require('../images/Community.png')} style={styles.Icon}/>
+            ),
+          }}
+          />
+        <Tab.Screen 
+          name="Setting" 
+          component={Setting} 
+          options={{
+            tabBarIcon: () => (
+              <Image source={require('../images/Setting.png')} style={styles.Icon}/>
+            ),
+          }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  Icon: {
+    width: 90 / 3,
+    flex: 0.8,
+  }
+});
 
 export default BreakoutApp;
